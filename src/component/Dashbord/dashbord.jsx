299764b9b3d0d4f33a9d21bf0c8cf250ea16abdc
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-03 17:34:07 +0530

    chart svg added

--- src/component/Dashbord/dashbord.jsx
+++ src/component/Dashbord/dashbord.jsx
@@ -4,12 +4,13 @@ import Star from '../../assets/Cards/Star.svg'
 import Newtask from '../../assets/Cards/NewTask.svg'
 import Projectdone from '../../assets/Cards/Projectdone.svg'
 import GlowLineChart from "../Charts/Glowlinecharts"
+import Graphline from "../../assets/Cards/Graph1.svg";
 
 export default function Dashbord() {
     const TaskTrackData = [
-        { id: 1,img:Star,title: "Task Complated" },
-        { id: 2,img:Newtask,title: "New Task" },
-        { id: 3,img:Projectdone,title: "Project Done"},
+        { id: 1,img:Star,title: "Task Complated",projectdetails:<>10+ more <br /> from last week</> },
+        { id: 2,img:Newtask,title: "New Task",projectdetails:<>5+ more <br /> from last week</> },
+        { id: 3,img:Projectdone,title: "Project Done",projectdetails:<>2+ more <br /> from last week</> },
     ]
 
     return (
@@ -26,11 +27,12 @@ export default function Dashbord() {
                                     <div className="col task-card">
                                         <div className="task-card-head ">
                                             <img src={task.img}/>
-                                            <p>{task.title}</p>
-                                            <div className="bb">
-                                            <GlowLineChart />
-                                            </div>
+                                            <p>{task.title}</p>                
                                         </div>
+                                        <div className="bb d-flex">
+                                               <img src={Graphline} alt="" />
+                                                <p className="">{task.projectdetails}</p>
+                                            </div>
                                     </div>
                                 ))}
                             </div>
