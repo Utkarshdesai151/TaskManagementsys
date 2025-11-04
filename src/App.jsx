130aa07f2bf44afcba9510e0b48365dbcbee2edc
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-04 17:45:01 +0530

    this is task list componante

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 "use client";
 import "../src/App.css";
 import Sidebar from "./component/sidebar";
-import Userpannel from "./component/userpannel";
 import Dashbord from "./component/Dashbord/dashbord";
+
 function App() {
   return (
-    <div>
-       <div className="app-container d-flex">
-      <div className="sidebar-container">
-        <Sidebar />
-      </div>
+    <div className="app-wrapper">
+      <div className="d-flex flex-row h-100">
+        {/* Sidebar Section */}
+        <aside className="sidebar-container">
+          <Sidebar />
+        </aside>
 
-      <div className="main-container flex-grow-1">
-        {/* <div className="userpannel-section mb-3">
-          <Userpannel />
-        </div> */} 
-        <div className="dashboard-section h-100">
+        {/* Main Content Section */}
+        <main className="main-content flex-grow-1">
           <Dashbord />
-        </div>
+        </main>
       </div>
     </div>
-    </div>
-
   );
 }
 
--- src/component/Dashbord/dashbord.jsx
+++ src/component/Dashbord/dashbord.jsx
@@ -7,37 +7,46 @@ import GlowLineChart from "../Charts/Glowlinecharts"
 import Graphline1 from "../../assets/Cards/Graph1.svg";
 import Graphline2 from "../../assets/Cards/Graph2.svg";
 import Graphline3 from "../../assets/Cards/Graph3.svg";
-
+import TaskChart from "../Taskchart.js/taskchart"
+import Tasklist from "../Tasklist/Tasklist"
 export default function Dashbord() {
     const TaskTrackData = [
-        { id: 1,img:Star,graphimg:Graphline1,title: "Task Complated",projectdetails:<>10+ more <br /> from last week</> },
-        { id: 2,img:Newtask,graphimg:Graphline2,title: "New Task",projectdetails:<>5+ more <br /> from last week</> },
-        { id: 3,img:Projectdone,graphimg:Graphline3,title: "Project Done",projectdetails:<>2+ more <br /> from last week</> },
+        { id: 1, img: Star, graphimg: Graphline1, title: "Task Complated", projectdetails: <>10+ more <br /> from last week</> },
+        { id: 2, img: Newtask, graphimg: Graphline2, title: "New Task", projectdetails: <>5+ more <br /> from last week</> },
+        { id: 3, img: Projectdone, graphimg: Graphline3, title: "Project Done", projectdetails: <>2+ more <br /> from last week</> },
     ]
 
     return (
-        <div className="main-user">
+        <div className="main-user container">
             <div className="userpannel-section mb-3 flex-grow-1">
                 <Userpannel />
             </div>
             <div className="container justify-content-center align-items-center">
                 <div className="row row-cols-2">
-                    <div className="col-8 ">
+                    <div className="col-9">
                         <section className="task-section px-4">
                             <div className="row row-cols-3 gap-3 flex">
                                 {TaskTrackData.map((task) => (
                                     <div className="col task-card">
                                         <div className="task-card-head ">
-                                            <img src={task.img}/>
-                                            <p>{task.title}</p>                
+                                            <img src={task.img} />
+                                            <p>{task.title}</p>
                                         </div>
                                         <div className="bb d-flex">
-                                               <img src={task.graphimg} alt="" />
-                                                <p className="">{task.projectdetails}</p>
-                                            </div>
+                                            <img src={task.graphimg} alt="" />
+                                            <p className="">{task.projectdetails}</p>
+                                        </div>
                                     </div>
                                 ))}
                             </div>
+                        </section>
+
+                        <section className="my-3 mx-1">
+                            <TaskChart />
+                        </section>
+                        
+                        <section className="my-4 mx-4">
+                            <Tasklist/>
                         </section>
                     </div>
                 </div>
