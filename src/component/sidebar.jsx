1aaa68ae5dfeda0d7ba3dc2676ca010d3e5edfc4
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-05 17:48:29 +0530

    task-list updated code clean fix responsiveness

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo2 from "../assets/logo/Group 9.svg";
 import Dashbord from "../assets/sidebar/Dashbord.svg";
 import Timeline from "../assets/sidebar/Timeline.svg";
@@ -9,23 +9,43 @@ import Messages from "../assets/sidebar/Message.svg";
 import "./sidebar.css";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   return (
-    <div className="sidebar sidebar-dark border-end">
-      <div className="text-center">
-        <img src={logo2} alt="Secondary Logo" width="45" height="45" />
-        <div className="fw-bold mt-2" style={{ fontSize: "16px" }}>
-          Octom
+    <>
+      {/* Hamburger Button */}
+      <button
+        className="hamburger-btn d-lg-none"
+        onClick={toggleSidebar}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+
+      {/* Sidebar */}
+      <div className={`sidebar sidebar-dark border-end ${isOpen ? "open" : ""}`}>
+        <div className="text-center">
+          <img src={logo2} alt="Secondary Logo" width="45" height="45" />
+          <div className="fw-bold mt-2" style={{ fontSize: "16px" }}>
+            Octom
+          </div>
         </div>
+
+        <ul className="sidebar-nav">
+          <li><img src={Dashbord} alt="Dashboard" /></li>
+          <li><img src={Timeline} alt="Timeline" /></li>
+          <li><img src={Task} alt="Task" /></li>
+          <li><img src={Setting} alt="Settings" /></li>
+          <li><img src={Messages} alt="Messages" /></li>
+          <li><img src={Folders} alt="Folders" /></li>
+        </ul>
       </div>
 
-      <ul className="sidebar-nav">
-        <li><img src={Dashbord} alt="Dashboard" /></li>
-        <li><img src={Timeline} alt="Timeline" /></li>
-        <li><img src={Task} alt="Task" /></li>
-        <li><img src={Setting} alt="Settings" /></li>
-        <li><img src={Messages} alt="Messages" /></li>
-        <li><img src={Folders} alt="Folders" /></li>
-      </ul>
-    </div>
+      {/* Overlay for small screens */}
+      {isOpen && <div className="sidebar-overlay" onClick={toggleSidebar}></div>}
+    </>
   );
 }
