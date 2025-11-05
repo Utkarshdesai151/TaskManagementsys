310d6e3b0ca22ab04526fcfcbd9dd3e314ab2c2c
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-05 17:56:54 +0530

    create task component

---
+++ src/component/schedule/schedule.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function  Todayschedule(){
+    return(
+        <div>
+            <div className="scheduler-container border">
+                <div className="schedle-head flex">
+                    <h1>Today's Schedule</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todayschedule;
