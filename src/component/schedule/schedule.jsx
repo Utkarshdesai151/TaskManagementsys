ca6e6812ad0dac70f0687aee2671608c1602ff63
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-07 17:09:13 +0530

    message

--- src/component/schedule/schedule.jsx
+++ src/component/schedule/schedule.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import DateIcon from "../../assets/Schedule/Date.svg";
 import Ic1 from "../../assets/Schedule/Ic1.svg";
-import './schedule.css'
+import "./schedule.css";
+import CallBar from "../Callbar";
+import Message from "../Message/Message";
 
 function Todayschedule() {
   return (
-    <div className="container p-3 ">
-      {/* Header Section */}
+    <div className="container p-3">
       <div className="d-flex justify-content-between align-items-center mb-3">
-        <p className="mb-0 fw-bold fs-5">Today's Schedule</p>
-        <div className="d-flex bg-dark px-3 py-2 rounded align-items-center">
-          <img src={Ic1} alt="icon" width="20" height="20" className="me-2" />
-          <img src={DateIcon} alt="date" width="20" height="20" />
+        <p className="mb-0">Today's Schedule</p>
+        <div className="d-flex bg-black gap-1 px-3 py-2 rounded-2 align-items-center">
+          <img src={Ic1} alt="icon" width="15" height="15" className="me-2" />
+          <img src={DateIcon} alt="date" width="15" height="15" />
         </div>
       </div>
 
-      {/* Schedule Item */}
-      <div className=" bg-opacity-25 border-0 p-3">
-        <div className="d-flex justify-content-between align-items-center">
-          <p className="mb-0 text-custom-blue  fw-semibold">
-            30-minute call with Client
-          </p>
-          <button className="btn btn-outline-primary btn-sm">+ Invite</button>
+      <div className="d-flex justify-content-between py-3">
+        <div>
+          <p className="text-custom-blue mb-1">30-minute call with Client</p>
+          <p className="mb-0">Project Discovery Call</p>
         </div>
+        <p className="mb-3 text-custom-blue">+ Invite</p>
       </div>
+      <CallBar />
+      <hr></hr>
+      <Message />
     </div>
   );
 }
