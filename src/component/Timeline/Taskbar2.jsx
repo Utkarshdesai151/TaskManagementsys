072c2ecde434c7cc6f93a9537acff91fe648ae41
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-11-10 18:00:59 +0530

    user pannel added

--- src/component/Timeline/Taskbar2.jsx
+++ src/component/Timeline/Taskbar2.jsx
@@ -1,94 +1,97 @@
 "use client";
 import React from "react";
+import Userpannel from "../userpannel";
 import "./taskbord.css";
 import { FaPlus } from "react-icons/fa";
 
 const tasks = [
-  {
-    id: 1,
-    title: "UX Research",
-    color: "#4C6EF5",
-    iconBg: "#4C6EF5",
-    members: [],
-    subtasks: [],
-  },
-  {
-    id: 2,
-    title: "Information Architecture",
-    color: "#845EF7",
-    iconBg: "#845EF7",
-    members: [],
-    subtasks: [],
-  },
-  {
-    id: 3,
-    title: "Design Phase",
-    color: "#495057",
-    iconBg: "#495057",
-    members: [],
-    subtasks: ["Build Wireframe", "User Interface D.."],
-  },
-  {
-    id: 4,
-    title: "Prototyping",
-    color: "#FCC419",
-    iconBg: "#FCC419",
-    members: [],
-    subtasks: [],
-  },
-  {
-    id: 5,
-    title: "Development",
-    color: "#ADB5BD",
-    iconBg: "#ADB5BD",
-    members: [],
-    subtasks: ["Back-End Dev..", "Front-End Dev.."],
-  },
+    {
+        id: 1,
+        title: "UX Research",
+        color: "#4C6EF5",
+        iconBg: "#4C6EF5",
+        members: [],
+        subtasks: [],
+    },
+    {
+        id: 2,
+        title: "Information Architecture",
+        color: "#845EF7",
+        iconBg: "#845EF7",
+        members: [],
+        subtasks: [],
+    },
+    {
+        id: 3,
+        title: "Design Phase",
+        color: "#495057",
+        iconBg: "#495057",
+        members: [],
+        subtasks: ["Build Wireframe", "User Interface D.."],
+    },
+    {
+        id: 4,
+        title: "Prototyping",
+        color: "#FCC419",
+        iconBg: "#FCC419",
+        members: [],
+        subtasks: [],
+    },
+    {
+        id: 5,
+        title: "Development",
+        color: "#ADB5BD",
+        iconBg: "#ADB5BD",
+        members: [],
+        subtasks: ["Back-End Dev..", "Front-End Dev.."],
+    },
 ];
 
 export default function TaskBoard() {
-  return (
-    <div className="task-board-container">
-      <div className="week-header">
-        {["S 04", "S 05", "S 06", "S 07", "S 08"].map((w, i) => (
-          <span key={i} className="week-text">
-            {w}
-          </span>
-        ))}
-      </div>
+    return (
+        <div>
+            <div className="task-board-container">
+                <div className="week-header">
+                    {["S 04", "S 05", "S 06", "S 07", "S 08"].map((w, i) => (
+                        <span key={i} className="week-text">
+                            {w}
+                        </span>
+                    ))}
+                </div>
 
-      <div className="task-list">
-        {tasks.map((task) => (
-          <div key={task.id} className="task-card">
-            <div className="task-header">
-              <div
-                className="task-icon"
-                style={{ backgroundColor: task.iconBg }}
-              >
-                <FaPlus color="#fff" size={10} />
-              </div>
-              <span className="task-title">{task.title}</span>
-              <div className="avatars">
-                {task.members.map((m, i) => (
-                  <div key={i} className="avatar">
-                    {m}
-                  </div>
-                ))}
-              </div>
+                <div className="task-list">
+                    {tasks.map((task) => (
+                        <div key={task.id} className="task-card">
+                            <div className="task-header">
+                                <div
+                                    className="task-icon"
+                                    style={{ backgroundColor: task.iconBg }}
+                                >
+                                    <FaPlus color="#fff" size={10} />
+                                </div>
+                                <span className="task-title">{task.title}</span>
+                                <div className="avatars">
+                                    {task.members.map((m, i) => (
+                                        <div key={i} className="avatar">
+                                            {m}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+
+                            {task.subtasks.length > 0 && (
+                                <div className="subtasks">
+                                    {task.subtasks.map((sub, i) => (
+                                        <div key={i} className="subtask">
+                                            {sub}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
-
-            {task.subtasks.length > 0 && (
-              <div className="subtasks">
-                {task.subtasks.map((sub, i) => (
-                  <div key={i} className="subtask">
-                    {sub}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
